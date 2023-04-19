726871cc8e4b3882205da9d2e4243f389382907b
Mirkhojiddin <71446322+Mirxojiddin@users.noreply.github.com>
2023-04-19 13:29:42 +0500

    Javob 4

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,5 +1,5 @@
-package com.epam.mjc.io;
 
+package com.epam.mjc.io;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -15,20 +15,17 @@ public class FileReader {
             int son = 0;
             int age=0;
             long phone=0;
-        try(FileInputStream fileInputStream = new FileInputStream(file.getAbsolutePath())) {
+            try(FileInputStream fileInputStream = new FileInputStream(file.getAbsoluteFile())) {
             int ch = fileInputStream.read();
-            
+        
             while(ch != -1) {
-                if ((char)ch=='\n'){
+                if ((char)ch=='\n' || (char)ch==' '){
+                    if ((char)ch=='\n'){
                     newStr[son++] = str;
                     str = "";
+                    }
                     ch = fileInputStream.read();
                     continue;
-                }
-                if ((char)ch==' '){
-                    ch = fileInputStream.read();
-                    continue;
-            
                 }
                 str += (char)ch;
                 ch = fileInputStream.read();
@@ -40,16 +37,25 @@ public class FileReader {
             System.out.println("I/O error");
         }
         for (int i=0; i<4; i++){
-            String[] arrOfStr = str.split(":", 2);
+            String[] arrOfStr = newStr[i].split(":", 2);
+            arrOfStr[1] = arrOfStr[1].trim();
             if (arrOfStr[0]!= null && arrOfStr[0].equals("Name") )
                 name = arrOfStr[1];
-            if (arrOfStr[0]!= null && arrOfStr[0].equals("Age"))
-                age = Integer.parseInt(arrOfStr[1]);
+            if (arrOfStr[0]!= null && arrOfStr[0].equals("Age")){
+                age = Integer.valueOf(arrOfStr[1]);
+            }
             if (arrOfStr[0]!= null && arrOfStr[0].equals("Email"))
                 email = arrOfStr[1];
-            if (arrOfStr[0]!= null && arrOfStr[0].equals("Phone"))
+            if (arrOfStr[0]!= null && arrOfStr[0].equals("Phone")){
                 phone = Long.parseLong(arrOfStr[1]);
+            }    
         }
+        StringBuilder sname = new StringBuilder(name);
+        StringBuilder semail = new StringBuilder(email);
+
         return new Profile(name,age,email,phone);
+    }
+    public static void main(String[] args) {
+        
     }
 }

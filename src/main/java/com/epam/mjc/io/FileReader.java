f6a5f430fec476d06fe26414ad8f6a2cbc6ae151
Mirkhojiddin <71446322+Mirxojiddin@users.noreply.github.com>
2023-04-19 17:19:48 +0500

    Javob 7

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,38 +1,35 @@
 package com.epam.mjc.io;
+
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.BufferedReader;
-import java.io.InputStreamReader;;
+import java.io.InputStreamReader;
 
 public class FileReader {
     public Profile getDataFromFile(File file) {
-        String name = "";
-        String email = "";
+        String name;
+        String email;
         String[] newStr = new String[4];
         int son = 0;
-        int age = 0;
-        long phone = 0;
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file.getAbsoluteFile())))) {
+        int age;
+        long phone;
+        try (BufferedReader br = new BufferedReader(
+                new InputStreamReader(new FileInputStream(file.getAbsoluteFile())))) {
             String line = br.readLine();
             while (line != null) {
-				newStr[son++] = line;
-				line = br.readLine();
-			}
-            
-        } catch (FileNotFoundException e) {
-            System.err.println(e);
-            // exception handling
+                newStr[son++] = line;
+                line = br.readLine();
+            }
         } catch (IOException e) {
-            System.err.println(e);
+            System.out.println("I/O Exception");
         }
-        String[] arrOfStr= newStr[0].split(":");
+        String[] arrOfStr = newStr[0].split(":");
         arrOfStr[1] = arrOfStr[1].trim();
         name = arrOfStr[1];
         arrOfStr = newStr[1].split(":");
         arrOfStr[1] = arrOfStr[1].trim();
-        age = Integer.valueOf(arrOfStr[1]);
+        age = Integer.parseInt(arrOfStr[1]);
         arrOfStr = newStr[2].split(":");
         arrOfStr[1] = arrOfStr[1].trim();
         email = arrOfStr[1];
